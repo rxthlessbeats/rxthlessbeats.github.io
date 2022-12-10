0cdaf740bc2c148bf0924c5ec9a0243641f9c520
David Wu <0830david@gmail.com>
2022-12-10 21:18:52 +0800

    try

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ import './home.scss';
 import Header from './global/header';
 function HomePage () {
     return (
-        <div className="Home" onClick={() => {window.location.href='/filter'}}>
+        <div className="Home" onClick={() => {window.location.href='/Job-Searching-Web/filter'}}>
             <Header/>
             <div className="content">
                 <img className="pic" src={require("../images/勞工處網站封面.png")} alt="勞工處網站封面"></img>
