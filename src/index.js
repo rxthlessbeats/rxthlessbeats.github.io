3d59cd8108a656c6472d6f479dc7242838cb9b4e
David Wu <0830david@gmail.com>
2022-12-11 16:28:43 +0800

    12110428

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import { HashRouter } from "react-router-dom";
 
 import "@fontsource/inter";
 
